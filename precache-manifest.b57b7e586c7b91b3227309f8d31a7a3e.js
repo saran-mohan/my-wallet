aaf5716889781b4a2ed205b3ca4325c5d1cea502
Saranyan M <saranyan.m@happiestminds.com>
2020-06-25 19:15:03 +0530

    Updates

---
+++ precache-manifest.b57b7e586c7b91b3227309f8d31a7a3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79b4f5d91650a1d0f4c715bdc713c94",
+    "url": "/my-wallet/index.html"
+  },
+  {
+    "revision": "13ca7f706b9f77b4d40e",
+    "url": "/my-wallet/static/css/main.61e62916.chunk.css"
+  },
+  {
+    "revision": "85561584b2668d7e542f",
+    "url": "/my-wallet/static/js/2.b133fbb1.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/my-wallet/static/js/2.b133fbb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ca7f706b9f77b4d40e",
+    "url": "/my-wallet/static/js/main.980ccd49.chunk.js"
+  },
+  {
+    "revision": "3cb645c6c6fbc858aa16",
+    "url": "/my-wallet/static/js/runtime-main.35883422.js"
+  },
+  {
+    "revision": "9f4f7fa745136c77ea886c6070bb7485",
+    "url": "/my-wallet/static/media/background.9f4f7fa7.jpg"
+  }
+]);
